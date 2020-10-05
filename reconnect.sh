503d6ce2820d84176477895bb0437e167b78c6a1
220242 <220242@gmail.com>
2020-10-05 09:27:17 +0300

    Create reconnect.sh

---
+++ reconnect.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#sudo crontab -e
+#*/1 * * * * /home/pi/reconnect.sh
+router=`ip route | awk '/default/ {print $3}'`
+/bin/ping -q -c5 1.1.1.1 > /dev/null
+
+if [ $? -eq  0 ]
+then
+  true
+  echo "Network OK"
+else
+  echo "Network down, fixing..."
+  # ifdown --force wlan1
+  # sleep 5
+        #  /bin/kill -9 `pidof wpa_supplicant`
+        #  /sbin/ifup --force wlan1
+        #  /sbin/ip route add default via $router dev wlan0
+        #  /bin/mount -a
+        sh /home/pi/random-mac-wlan1.sh
+        curl --header 'Content-Type: application/json' --request 'POST' --data '{"chat_id":"594415350","text":"wlan1 re$  echo "wlan1 reconnected at `date`"
+fi
